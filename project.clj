206ca24ade50dc91211fc2ec8ececbb5a724af36
Tomas Kacur <tomas.kacur@keboola.com>
2018-02-21 11:37:49 +0800

    update deps: clojure to 1.9.0, core.async to 0.4.474

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/kacurez/clj-parallels"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/core.async "0.3.443"]
-                 [org.clojure/clojure "1.9.0-alpha17"]]
+  :dependencies [[org.clojure/core.async "0.4.474"]
+                 [org.clojure/clojure "1.9.0"]]
   :repositories [["snapshots" {:url "https://clojars.org/repo"
                                :username :env/clojars_username
                                :password :env/clojars_password}]
